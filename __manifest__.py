f79c74895e5cbd537102c5a9ffa41260a4688c78
LuyangXing <leon.luyangxing@gmail.com>
2018-03-21 20:28:26 +0800

    ver 0.2
    Part 01 - Personnel

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "RW Headhunter",
+    'name': "RW Mod - Headhunter",
 
     'summary': """
-        Module 1001
+        Module - 101
     """,
 
     'description': """
@@ -16,20 +16,27 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'RuiWen',
-    'version': '0.1',
+    'category': 'ReiWen',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base',
+                'mail',
+                'test_impex',
+                'rw_b_repoweredby',
+                'rw_b_location',
+                ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/personnel.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
 }
