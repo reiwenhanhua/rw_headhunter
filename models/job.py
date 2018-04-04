7befb9d7140283b43190534a4a2e3385ef3213cb
LuyangXing <leon.luyangxing@gmail.com>
2018-04-04 17:52:38 +0800

    Ver 1.0.0.2

--- models/job.py
+++ models/job.py
@@ -8,3 +8,21 @@ class Job(models.Model):
     _inherit = ['rw_headhunter.baseattribute']
 
     color = fields.Integer("Color Index")
+
+    state = fields.Selection([
+        ('recruit', 'Recruitment in Progress'),
+        ('open', 'Not Recruiting')
+    ], string='Status', readonly=True, required=True, track_visibility='always', copy=False, default='recruit',
+        help="Set whether the recruitment process is open or closed for this job position.")
+
+    @api.multi
+    def set_recruit(self):
+        for record in self:
+            record.write({'state': 'recruit'})
+        return True
+
+    @api.multi
+    def set_open(self):
+        return self.write({
+            'state': 'open',
+        })
--- models/match.py
+++ models/match.py
@@ -13,3 +13,20 @@ class Match(models.Model):
     personnel_id = fields.Many2one('rw_headhunter.personnel', "Personnel")
     job_duty_id = fields.Many2one('rw_headhunter.jobduty', "Job Duty")
     trade_id = fields.Many2one('rw_headhunter.trade', "Trade")
+
+    date_action = fields.Date("Next Action Date")
+
+    @api.multi
+    def action_makemeeting(self):
+        """ This opens Meeting's calendar view to schedule meeting on current applicant
+            @return: Dictionary value for created Meeting view
+        """
+        self.ensure_one()
+
+        category = self.env.ref('rw_headhunter.match_meet_interview')
+        res = self.env['ir.actions.act_window'].for_xml_id('calendar', 'action_calendar_event')
+        res['context'] = {
+            # 'default_user_id': self.env.uid,
+            # 'default_name': self.name,
+        }
+        return res
